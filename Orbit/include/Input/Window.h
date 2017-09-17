f382f0119473dc7706124b5c3602167b8de9ca88
Benoit Paradis <benoit.paradis15@gmail.com>
2017-09-17 17:56:02 -0400

    Done documentation for Issue 6

--- Orbit/include/Input/Window.h
+++ Orbit/include/Input/Window.h
@@ -12,51 +12,101 @@ struct GLFWwindow;
 
 namespace Orbit
 {
-	class Renderer;
+	class Renderer; /// Forward declaration for the Renderer class.
 
+	/// A class encapsulating windowing operations and allows for running work load on the side.
+	/// This class implements the Singleton pattern, meaning its one instance should be acquired using
+	/// getInstance(). Furthermore, before acquiring an instance, it should be initialized to a correct state
+	/// using createWindow().
 	class Window final
 	{
 	public:
+		/// Sets the window singleton to a window with the defined parameters.
+		/// @param width the width of the window, in pixels.
+		/// @param height the height of the window, in pixels.
+		/// @param title the name of the window, which turns up in the task bar.
+		/// @param fullscreen whether or not the window should be fullscreen on startup.
 		static void createWindow(int width, int height, const std::string& title, bool fullscreen = false);
+
+		/// Returns the one instance of this class, as per the Singleton design pattern.
+		/// @return the single window instance.
 		static Window& getInstance();
 
+		/// Destroys the window. Also cleans up the GLFW library.
 		~Window();
 
+		/// Opens the window. This has the effect of acquiring resources to open the physical window and then
+		/// opening it to display things to the user. This must be called before calling other member methods,
+		/// as otherwise their behaviour will be undefined.
 		void open();
+		
+		/// Closes the window. Useful in other threads where it is necessary to signal that the program should
+		/// end. Care should be taken that other threads exit in a proper manner before calling this method,
+		/// as it usually will mean that the main thread will exit (leaving the program with orphaned threads).
 		void close();
 
+		/// Changes the window's state from fullscreen to windowed.
+		/// @param value true if fullscreen, false if windowed.
 		void setFullscreen(bool value);
 
-		void setShouldClose(bool value);
+		/// Returns whether or not the window has been flagged as 'should close'.
+		/// @return whether or not the window should close.
 		bool shouldClose() const;
 
+		/// Returns the renderer built alongside the window. The renderer allows graphics to be drawn on the
+		/// Window surface. Renderers can be changed at compile-time. Returns a raw pointer as an
+		/// std::observer_ptr type is unavailable.
+		/// @return the renderer for the window.
 		Renderer* getRenderer() const;
 
+		/// Defines whether or not the updating loop should be capped in terms of calls per second.
+		/// This allows fine-tuning performance requirements for the thread in which the window will run.
+		/// Setting this will attempt to make the thread sleep until the next scheduled time (determined by
+		/// the ticks per second, defined by setTargetTicksPerSecond()).
+		/// @param value whether or not to constrain ticking.
 		void constrainTicking(bool value);
-		void setTargetTicksPerSecond(int targetTicksPerSecond);
 
+		/// Determines the amount of ticks per second the window thread (when calling run()) should attempt to
+		/// cap itself to allow more processing time for other tasks. Has no effect if ticking hasn't been
+		/// constrained.
+		/// @param targetTicksPerSecond the target amount of ticks every second, in Hz.
+		void setTargetTicksPerSecond(unsigned int targetTicksPerSecond);
+
+		/// A typedef simplifying the use of nanosecond-using function pointers.
 		using TickingCallback = void(*)(std::chrono::nanoseconds elapsedTime);
+
+		/// Takes control of the current thread and executes callbacks to the callback in parameter. To 
+		/// control behaviour, use the contrainTicking() and setTargetTicksPerSecond() methods. Before 
+		/// using the callback, handles all OS messages.
+		/// @param tickingCallback the callback to run every tick.
 		void run(TickingCallback tickingCallback);
 
 	private:
+		/// Default constructor for the class. Called once, to set up an invalid window as the singleton at
+		/// the start of the program.
 		Window() = default;
+
+		/// Main constructor for the class. Called when createWindow() is called - it is only used to update
+		/// the instance of the Singleton.
 		Window(int width, int height, const std::string& title, bool fullscreen);
 
 		Window(const Window&) = default;
 		Window& operator=(const Window&) = default;
 
-		GLFWwindow* _windowHandle = nullptr;
+		GLFWwindow* _windowHandle = nullptr; ///< The handle of the window, using GLFW.
 
-		int _width;
-		int _height;
+		int _width; ///< The width of the window.
+		int _height; ///< The height of the window.
 
-		bool _tickingConstrained = false;
-		int _targetTicksPerSecond = 60;
+		bool _tickingConstrained = false; ///< Whether or not the window should attempt to tick as fast as possible.
+		unsigned int _targetTicksPerSecond = 60; ///< The target amount of ticks per second.
 
-		bool _fullscreen;
-		std::string _title;
+		bool _fullscreen; ///< Whether or not the window is currently in fullscreen mode.
+		std::string _title; ///< The title of the window.
 
-		static Window _instance;
+		bool _running = false;
+
+		static Window _instance; ///< The instance of the window.
 	};
 }
 
--- Orbit/src/Input/Window.cpp
+++ Orbit/src/Input/Window.cpp
@@ -7,16 +7,29 @@
 
 using namespace Orbit;
 
+/// The implementation of the window's static instance.
 Window Window::_instance;
 
+/// Whether or not the window has been created already using createWindow(). This is useful as getInstance()
+/// will fail if createWindow() hasn't been called once (and the window is in an invalid state). Ensures that
+/// the program will fail fast and not silently.
 static bool _windowCreated = false;
 
+/// Essentially sets the window singleton's instance by calling the appropriate (private) constructor.
+/// Also sets the flag indicating that the window was created at least once.
+/// @param width the width of the window.
+/// @param height the height of the window.
+/// @param title the title of the window
+/// @param fullscreen whether or no the window should be created in fullscreen.
 void Window::createWindow(int width, int height, const std::string& title, bool fullscreen)
 {
 	_instance = Window(width, height, title, fullscreen);
 	_windowCreated = true;
 }
 
+/// Returns the window's instance, per the Singleton design pattern. Fails fast if createWindow() hasn't been
+/// called at least once.
+/// @return the window single instance.
 Window& Window::getInstance()
 {
 	if (!_windowCreated)
@@ -25,11 +38,19 @@ Window& Window::getInstance()
 	return _instance;
 }
 
+/// The primary constructor for the window. Only sets the class's members - resource acquisition is done in
+/// open() as to have a valid this pointer (and to simplify window show/hide code).
+/// @param width the width of the window.
+/// @param height the height of the window.
+/// @param title the title of the window.
+/// @param fullscreen whether or not the window should be created fullscreen.
 Window::Window(int width, int height, const std::string& title, bool fullscreen)
 	: _width(width), _height(height), _title(title), _fullscreen(fullscreen)
 {
 }
 
+/// The destructor for the class. Destroys the window handle is appropriate. Also terminates the GLFW
+/// library (it will be be reinitiated when the next window is opened, if any).
 Window::~Window()
 {
 	if (_windowHandle != nullptr)
@@ -38,6 +59,8 @@ Window::~Window()
 	glfwTerminate();
 }
 
+/// Opens the window. Begins by initiating the GLFW library, sets the hints for the window creation and sets
+/// callbacks. Finally creates the window.
 void Window::open()
 {
 	if (!glfwInit())
@@ -51,16 +74,24 @@ void Window::open()
 	glfwSetWindowUserPointer(_windowHandle, this);
 }
 
+/// "Closes" the window. Essentially sets the 'shouldClose' flag so the window is closed on the next run
+/// loop. Throws an exception if the window wasn't opened.
 void Window::close()
 {
 	if (_windowHandle == nullptr)
 		throw std::runtime_error("Cannot close a window that was not opened!");
 
-	setShouldClose(true);
+	glfwSetWindowShouldClose(_windowHandle, true);
 }
 
+/// Sets the window to a fullscreen (or windowed, depending on the parameter) mode. Throws an exception if
+/// the window wasn't opened.
+/// @param value the value of the fullscreen flag (true for fullscreen, false for windowed).
 void Window::setFullscreen(bool value)
 {
+	if (_windowHandle == nullptr)
+		throw std::runtime_error("Cannot change a state for a window that was not opened!");
+
 	_fullscreen = value;
 
 	if (_fullscreen)
@@ -75,41 +106,57 @@ void Window::setFullscreen(bool value)
 	}
 }
 
-void Window::setShouldClose(bool value)
-{
-	glfwSetWindowShouldClose(_windowHandle, value);
-}
-
+/// Returns whether or not the window should close. Simple syntactic sugar over the GLFW C API.
+/// @return whether or not the window should close.
 bool Window::shouldClose() const
 {
 	return glfwWindowShouldClose(_windowHandle);
 }
 
+/// Returns the renderer that was built with the window. Renderers are set at compile-time. Returns a raw
+/// pointer as std::observer_ptr is not available in VS2017 as of this commit.
+/// @return the renderer for the window.
 Renderer* Window::getRenderer() const
 {
 	// TODO
 	return nullptr;
 }
 
+/// Flags whether or not the window ticks its main loop at fastest speed or if it allows time for the
+/// thread to sleep.
+/// @param value the value of the flag.
 void Window::constrainTicking(bool value)
 {
 	_tickingConstrained = value;
 }
 
-void Window::setTargetTicksPerSecond(int targetTicksPerSecond)
+/// Determines the ideal amount of ticks each second for the main loop. Has no effect if constrainTicking has
+/// not been called with true as parameter.
+/// @param targetTicksPerSecond the target amount of ticks each second.
+void Window::setTargetTicksPerSecond(unsigned int targetTicksPerSecond)
 {
 	_targetTicksPerSecond = targetTicksPerSecond;
 }
 
+/// Contains the main loop of the window. Ensures the window is correctly initialized and then launches
+/// the main loop, calling the callback with the elapsed time between loops on every loop.
+/// This throws if the window is already running - it is intended to only be run once on the main loop.
+/// It also throws if the window has not been opened and if the callback is null.
+/// @param callback the callback to call every loop.
 void Window::run(TickingCallback callback)
 {
 	using namespace std::chrono;
+
+	if (_running)
+		throw std::runtime_error("The window is already running!");
 
 	if (_windowHandle == nullptr)
 		throw std::runtime_error("The window was not opened before running it!");
 
 	if (callback == nullptr)
 		throw std::runtime_error("Cannot run a window without a ticking callback!");
+
+	_running = true;
 
 	high_resolution_clock::time_point lastTime = high_resolution_clock::now();
 	while (!shouldClose())
